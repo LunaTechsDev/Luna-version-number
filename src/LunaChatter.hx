b20cecb090b5fd7a93174c6a4e00b0031f7ecd1c
Kino  Rose <kinoarose@gmail.com>
2020-09-10 21:09:22 -0400

    Initial commit

---
+++ src/LunaChatter.hx
@@ -0,0 +1,18 @@
+import utils.Parse;
+import core.Amaryllis;
+import pixi.interaction.EventEmitter;
+import rm.types.RM.TextState;
+import rm.abstracts.managers.AudioMgr;
+import rm.types.RPG.AudioFile;
+import rm.managers.AudioManager;
+import rm.windows.Window_Message;
+import utils.Comment;
+import core.Types.JsFn;
+import utils.Fn;
+import rm.core.Rectangle;
+import rm.Globals;
+
+class LunaChatter {
+ public static function main() {
+ }
+}
